5b47788d7d3ddcdae41abb1ce9b19c5264f32732
Gigabite Studio's LLC <89956790+OusmBlueNinja@users.noreply.github.com>
2022-10-08 10:54:06 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -223,12 +223,16 @@ if responce == None or responce == 'y' or responce == 'Y':
     (out, err) = proc.communicate()
     if err == None:
     #saves the output to an array so it can get the password and username from it
-      print(out)
-      uname = int(out.index('login: ')) + 1
-      host = int(out.index('host: ')) + 1
-      passkey = int(out.index('password: ')) + 1
+      #print(out)
+      if 'login: ' in out:
+        uname = int(out.index('login:')) + 1
+        print(out[uname])
+      else:
+          print('[ERROR]')
+      #host = int(out.index('host:')) + 1
+      #passkey = int(out.index('password:')) + 1
       
-      print(f'sshpass -p {passkey} {uname}@{host}')
+      #print(f'sshpass -p {passkey} {uname}@{host}')
       
       
     else:
