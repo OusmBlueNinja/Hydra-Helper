0279c4ce551c33496ce68b18833121a61fba0cd7
Gigabite Studio's LLC <89956790+OusmBlueNinja@users.noreply.github.com>
2022-10-08 11:42:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import subprocess
 
 import time
 import random
-from tkinter import EXCEPTION
 
 logo = '''
 ██╗░░██╗██╗░░░██╗██████╗░██████╗░░█████╗░░░░░░░██╗░░██╗███████╗██╗░░░░░██████╗░███████╗██████╗░
